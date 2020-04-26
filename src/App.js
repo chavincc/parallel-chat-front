8c9c1e0caabbba50609c1b068655e412a0126ad9
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-27 01:09:45 +0700

    feat(app): initialize project
    create react app and setup building blocks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/navbar';
+
+import Boards from './scenes/boards';
+import Login from './scenes/sign/scenes/login';
+import Register from './scenes/sign/scenes/register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/boards">
+            <Boards />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
