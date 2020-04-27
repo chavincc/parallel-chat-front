31c6be5aa327616bed74729346404d8d070db48d
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-28 00:40:20 +0700

    style(app): remove react.strict
    this project only care about backend

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import App from './App';
 import { AuthProvider } from './services/store/authStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
   document.getElementById('root')
 );
